3a2a6a6c30c506d2468058a1c4c698efec13f0ae
pedro-siqueira <43863697+pedro-siqueira@users.noreply.github.com>
2020-11-23 19:16:36 -0300

    classes Livro e Cliente criadas

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,28 @@
 public class Cliente {
     
+    private int id;
+    private String nome;
+    private String email;
+    private int cpf;
+    private int telefone;
+
+    public Cliente(){
+        this(-1,"","",-1,-1);
+    }
+
+    public Cliente(int id, String nome, String email, int cpf, int telefone) {
+        this.id = id; this.nome = nome; this.email = email; this.cpf = cpf; this.telefone = telefone;
+    }
+
+    public void setId(int id){ this.id = id; }
+    public int getId(){ return this.id; }
+    public void setNome(String nome){ this.nome = nome; }
+    public String getNome(){ return this.nome; }
+    public void setEmail(String email){ this.email = email; }
+    public String getEmail(){ return this.email; }
+    public void setCpf(int cpf){ this.cpf = cpf; }
+    public int getCpf(){ return this.cpf; }
+    public void setTelefone(int telefone){ this.telefone = telefone; }
+    public int getTelefone(){ return this.telefone; }
+
 }
